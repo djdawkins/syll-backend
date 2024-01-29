968eb5b432dbc2e2d756c18067bd11eb3513240f
djdawkins <ddawkns@gmail.com>
2024-01-28 20:49:00 -0500

    switched to loading local faiss index

--- langchain_app.py
+++ langchain_app.py
@@ -13,15 +13,10 @@ api_key = os.getenv('OPENAI_API_KEY')
 def  ask_question(question):
     llm = ChatOpenAI(openai_api_key=api_key, temperature=0.2)
 
-    # Load pdf and initiate
-    loader = PyPDFLoader("FA22_NURS-112 Sec 81 - Prof Prac Suc I sp 2024.pdf")
-    embeddings = OpenAIEmbeddings(openai_api_key=api_key)
-    docs = loader.load()
-
-    # Split text and create vector
-    text_splitter = RecursiveCharacterTextSplitter()
-    documents = text_splitter.split_documents(docs)
-    vector = FAISS.from_documents(documents, embeddings)
+    open_ai_embedding = OpenAIEmbeddings(openai_api_key=api_key)
+    db = FAISS.load_local("my_faiss_index", open_ai_embedding)
+    
+    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":9})
 
     # set prompt and create document chain
     prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:
@@ -32,21 +27,13 @@ def  ask_question(question):
 
     Question: {input}""")
 
+    # Doc chain
     document_chain = create_stuff_documents_chain(llm, prompt)
 
     # Retrieve
-    retriever = vector.as_retriever()
     retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
     # Get response
-    quiz_questions = ["When and where is the course meeting?", 
-                  "What are the required textbooks for the course?",
-                  "How will your final grade be calculated?",
-                  "What is the instructor's policy on late assignments?",
-                  "What is the attendance policy for the course?",
-                  "How can you access additional course materials and resources?",
-                  "What is the instructor's preferred method of communication?"]
-
     response = retrieval_chain.invoke({"input": question})
     response = str(response["answer"])
     # print(response["answer"])
